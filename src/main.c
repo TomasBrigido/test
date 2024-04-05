ab711f610b6befcd72fe3f1617aa3d7e94bb549f
TomasBrigido <tomas.brigido@mi.unc.edu.ar>
2024-04-05 12:47:07 -0300

    Commits test signature

--- src/main.c
+++ src/main.c
@@ -9,16 +9,13 @@ int main(int argc, char *argv[])
     printf("Hello, OS II. Welcome to 2024 course!\n");
     greetings();
     printf("Calling shared lib from main program. I get'%d'\n", shared_lib_function("Hello, OS II"));
-    printf("Calling static lib from main program. I get'%d'\n", static_lib_function("Bye, OS II"));
+  b from main program. I get'%d'\n", static_lib_function("Bye, OS II"));
 asdasd
 asdasd
 asd
 asd
 as
-das
-das
-das
-d
+da
 
     returnas
 das
